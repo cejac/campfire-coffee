eae1947b24e53e3bfc3e5e9dd13ca08ff5ae869d
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-06-11 11:25:05 -0700

    have stores printing

--- app.js
+++ app.js
@@ -85,3 +85,10 @@ for (idx in hours) {
 }
 
 table.appendChild(stores);
+
+
+for (var i = 0; i < allstores.length; i++) {
+  var locations = document.createElement('tr');
+  locations.textContent = allstores[i].kiosk;
+  table.appendChild(locations);
+};
