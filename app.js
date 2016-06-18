7ee2bad76b2026cb5fe3e6a6ed12faaf287d9bea
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-06-17 21:30:34 -0700

    new stores are now adding to both tables correclty

--- app.js
+++ app.js
@@ -53,6 +53,8 @@ Store.prototype.daily = function() {
   }
 };
 
+
+
 Store.prototype.render = function() {
   this.hourly();
   this.daily();
@@ -72,9 +74,8 @@ southLake.render();
 seaTac.render();
 
 //table head
-function beanTable() {
+function beanHeader() {
   var beTable = document.getElementById('beanTable');
-
   var stores = document.createElement('tr');
   var empty = document.createElement('th');
   var totals = document.createElement('th');
@@ -88,13 +89,31 @@ function beanTable() {
     tdEl.textContent = hours[idx];
     stores.appendChild(tdEl);
   }
-
   beTable.appendChild(stores);
 }
-beanTable();
+beanHeader();
+
+function baristaHeader() {
+  var baTable = document.getElementById('baristaTable');
+  var baStores = document.createElement('tr');
+  var baEmpty = document.createElement('th');
+  var baTotals = document.createElement('th');
+  baTotals.textContent = 'Daily Totals';
+  baEmpty.textContent = '';
+  baStores.appendChild(baEmpty);
+  baStores.appendChild(baTotals);
+
+  for (idx in hours) {
+    var baTdEl = document.createElement('th');
+    baTdEl.textContent = hours[idx];
+    baStores.appendChild(baTdEl);
+  }
+  baTable.appendChild(baStores);
+}
+baristaHeader();
 
 //table data
-function createBeanTable() {
+function beanTable() {
   for (var i = 0; i < allstores.length; i++) {
     var beTable = document.getElementById('beanTable');
     var locations = document.createElement('tr');
@@ -110,13 +129,31 @@ function createBeanTable() {
     beTable.appendChild(locations);
   };
 }
-createBeanTable();
+beanTable();
+
+function baristaTable() {
+  var baTable = document.getElementById('baristaTable');
+  for (var n = 0; n < allstores.length; n++) {
+    var baLocations = document.createElement('tr');
+    baLocations.textContent = allstores[n].kiosk;
+    var employeesTotalEl = document.createElement('td');
+    employeesTotalEl.textContent = allstores[n].employeesTotal;
+    baLocations.appendChild(employeesTotalEl);
+    for (j in hours) {
+      var empHour = document.createElement('td');
+      empHour.textContent = allstores[n].employeesNeeded[j];
+      baLocations.appendChild(empHour);
+    }
+    baTable.appendChild(baLocations);
+  };
+}
+baristaTable();
 
 //total row
 function beanTotal(){
   var beTable = document.getElementById('beanTable');
   var totalRow = document.createElement('tr');
-  totalRow.id = 'totalRow'
+  totalRow.id = 'totalRow';
 
   var totalTotalTd = document.createElement('td');
   totalTotalTd.textContent = 'Total';
@@ -143,7 +180,37 @@ function beanTotal(){
 }
 beanTotal();
 
+function baristaTotal() {
+  var baTable = document.getElementById('baristaTable');
+  var baTotalRow = document.createElement('tr');
+  baTotalRow.id = 'baTotalRow';
 
+  var baTotalTotalTd = document.createElement('td');
+  baTotalTotalTd.textContent = 'Total';
+  baTotalRow.appendChild(baTotalTotalTd);
+
+  baTable.appendChild(baTotalRow);
+
+  for (var o = 0; o < allstores.length; o++) {
+    dailyBarTotal += allstores[o].employeesTotal;
+    var dailyBarEl = document.createElement('td');
+    dailyBarEl.textContent = dailyBarTotal;
+    for (var p = 0; p < hours.length; p++) {
+      hourlyBarTotal[p] += allstores[o].employeesNeeded[p];
+    };
+  };
+
+  baTotalRow.appendChild(dailyBarEl);
+
+  for (b in hours) {
+    var baHourly = document.createElement('td');
+    baHourly.textContent = hourlyBarTotal[b];
+    baTotalRow.appendChild(baHourly);
+  };
+}
+baristaTotal();
+
+//Event handler
 var addStore = document.getElementById('newStore');
 addStore.addEventListener('submit', handleFormSubmit);
 
@@ -156,28 +223,48 @@ function handleFormSubmit() {
   var cups = parseFloat(event.target.cups.value);
   var toGo = parseFloat(event.target.toGo.value);
 
-  // document.getElementById('beanTable').remove();   //removes table, but need to rebuild
-
   var addNewStore = new Store(storeLocation, minCust, maxCust, cups, toGo);
-  var beTable = document.getElementById('beanTable');
-  var removeTr = document.getElementById('totalRow');
-  beTable.removeChild(removeTr);
 
   addNewStore.render();
+  function newBean() {
+    var beTable = document.getElementById('beanTable');
+    var removeTr = document.getElementById('totalRow');
+    beTable.removeChild(removeTr);
 
-  var locations = document.createElement('tr');
-  locations.textContent = addNewStore.kiosk;
-  var totalPoundsDayTdEl = document.createElement('td');
-  totalPoundsDayTdEl.textContent = addNewStore.totalPoundsDay;
-  locations.appendChild(totalPoundsDayTdEl);
-  for (j in hours) {
-    var hourBeans = document.createElement('td');
-    hourBeans.textContent = addNewStore.totalPoundsPerHour[j];
-    locations.appendChild(hourBeans);
+    var locations = document.createElement('tr');
+    locations.textContent = addNewStore.kiosk;
+    var totalPoundsDayTdEl = document.createElement('td');
+    totalPoundsDayTdEl.textContent = addNewStore.totalPoundsDay;
+    locations.appendChild(totalPoundsDayTdEl);
+    for (j in hours) {
+      var hourBeans = document.createElement('td');
+      hourBeans.textContent = addNewStore.totalPoundsPerHour[j];
+      locations.appendChild(hourBeans);
+    }
+    beTable.appendChild(locations);
+    beanTotal();
   }
-  beTable.appendChild(locations);
+  newBean();
 
-  beanTotal();
+  function newBarista() {
+    var baTable = document.getElementById('baristaTable');
+    var baRemoveTr = document.getElementById('baTotalRow');
+    baTable.removeChild(baRemoveTr);
+
+    var baLocations = document.createElement('tr');
+    baLocations.textContent = addNewStore.kiosk;
+    var employeesTotalEl = document.createElement('td');
+    employeesTotalEl.textContent = addNewStore.employeesTotal;
+    baLocations.appendChild(employeesTotalEl);
+    for (j in hours) {
+      var empHour = document.createElement('td');
+      empHour.textContent = addNewStore.employeesNeeded[j];
+      baLocations.appendChild(empHour);
+    }
+    baTable.appendChild(baLocations);
+    baristaTotal();
+  };
+  newBarista();
 
   event.target.kiosk.value = null;
   event.target.min.value = null;
@@ -185,72 +272,3 @@ function handleFormSubmit() {
   event.target.cups.value = null;
   event.target.toGo.value = null;
 };
-
-
-
-
-
-
-//Barista
-// var baHead = document.createElement('h2');
-// var baTable = document.createElement('table');
-// baHead.textContent = 'Baristas Needed By Location Each Day';
-// table.appendChild(baHead);
-// table.appendChild(baTable);
-
-// var baStores = document.createElement('tr');
-// var baEmpty = document.createElement('th');
-// var baTotals = document.createElement('th');
-// baTotals.textContent = 'Daily Totals';
-// baEmpty.textContent = '';
-// baStores.appendChild(baEmpty);
-// baStores.appendChild(baTotals);
-
-// for (idx in hours) {
-//   var baTdEl = document.createElement('th');
-//   baTdEl.textContent = hours[idx];
-//   baStores.appendChild(baTdEl);
-// }
-
-// baTable.appendChild(baStores);
-
-// //table data
-// for (var n = 0; n < allstores.length; n++) {
-//   var baLocations = document.createElement('tr');
-//   baLocations.textContent = allstores[n].kiosk;
-//   var employeesTotalEl = document.createElement('td');
-//   employeesTotalEl.textContent = allstores[n].employeesTotal;
-//   baLocations.appendChild(employeesTotalEl);
-//   for (j in hours) {
-//     var empHour = document.createElement('td');
-//     empHour.textContent = allstores[n].employeesNeeded[j];
-//     baLocations.appendChild(empHour);
-//   }
-//   baTable.appendChild(baLocations);
-// };
-
-// //total row
-// var baTotalRow = document.createElement('tr');
-
-// var baTotalTotalTd = document.createElement('td');
-// baTotalTotalTd.textContent = 'Total';
-// baTotalRow.appendChild(baTotalTotalTd);
-
-// baTable.appendChild(baTotalRow);
-
-// for (var o = 0; o < allstores.length; o++) {
-//   dailyBarTotal += allstores[o].employeesTotal;
-//   var dailyBarEl = document.createElement('td');
-//   dailyBarEl.textContent = dailyBarTotal;
-//   for (var p = 0; p < hours.length; p++) {
-//     hourlyBarTotal[p] += allstores[o].employeesNeeded[p];
-//   };
-// };
-
-// baTotalRow.appendChild(dailyBarEl);
-
-// for (b in hours) {
-//   var baHourly = document.createElement('td');
-//   baHourly.textContent = hourlyBarTotal[b];
-//   baTotalRow.appendChild(baHourly);
-// };
